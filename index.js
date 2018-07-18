84b09887af3d7a0acfa2f800d2f5c6ff71ebfce8
LBisca <lucas.simon.b@gmail.com>
2018-07-18 19:15:16 -0300

    Add User Input.

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const request = require('./node_modules/request');
+const yargs = require('./node_modules/yargs');
+
+const argv = yargs
+.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch for',
+        string: true
+    }
+})
+.help()
+.alias('help','h')
+.argv;
+
+var encodedAddress = encodeURIComponent(argv.a);
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=%20151%20Av%20Filipe%20Lobo%20Lauzane',
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
