ea6e4403f99dc512772c229a0c26ea2771884e1b
LBisca <lucas.simon.b@gmail.com>
2018-07-18 20:10:05 -0300

    Add Error handling.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const request = require('./node_modules/request');
 const yargs = require('./node_modules/yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
 .options({
@@ -11,16 +12,13 @@ const argv = yargs
     }
 })
 .help()
-.alias('help','h')
+.alias('help', 'h')    
 .argv;
 
-var encodedAddress = encodeURIComponent(argv.a);
-
-request({
-    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.a, (errorMessage, results) => {
+    if(errorMessage){
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2))
+    }
 });
